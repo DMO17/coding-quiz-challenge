d1cd515a731cae7ef5c021a6d9032c5a6afe1175
Dahir Mohamed <dmoabdi98@gmail.com>
2021-10-06 17:21:15 +0100

    validated the answers for question card in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
 const mainPage = document.querySelector(".main-page");
+const starterDiv = document.querySelector(".start-quiz-container");
 
-//set upp an event listner attached to the starterDiv to remove the page and start the timer when the button is clicked
+const questionAndAnswers = {};
+
+//set upp an event listener attached to the starterDiv to remove the page
 
 starterDiv.addEventListener("click", startBtn);
 
 function startBtn(event) {
-  const starterDiv = document.querySelector(".start-quiz-container");
   const target = event.target;
 
   if (target.getAttribute("class") === "start-btn") {
     starterDiv.remove();
+  }
+}
+
+// testing html question card for answer validation using console.log
+
+const divBox = document.querySelector(".question-1");
+
+divBox.addEventListener("click", onClick);
+
+function onClick(event) {
+  const target = event.target;
+  const currentTarget = event.currentTarget;
+
+  if (target.getAttribute("class") === "options") {
+    const dataText = target.getAttribute("data-message");
+    const dataMessage = currentTarget.getAttribute("data-main");
+    if (dataText === dataMessage) {
+      console.log("correct answer");
+      divBox.remove();
+    } else {
+      console.log("incorrect answer");
+      divBox.remove();
+    }
   }
 }
